85c067da21a303225d1daafa24518e983fec796b
chhetri28 <chettrisourav28@gmail.com>
2020-07-24 20:43:57 +0530

    Longest uncommon subsequences

---
+++ Longest Uncommon Subsequence.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define PB push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define ll long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl; 
+#define OJ \
+	freopen("input.txt", "r", stdin); \
+	freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main(){
+	OJ;
+	FIO;
+	string a,b;
+	cin>>a>>b;
+	if(a==b){
+		cout<<"-1\n";
+	}
+	else if(a.length()>b.length()){
+		cout<<a.length()<<endl;
+	}
+	else{
+		cout<<b.length()<<endl;
+	}
+}#include<bits/stdc++.h>
+using namespace std;
+
+typedef vector <int> vi;
+typedef pair< int ,int > pii;
+#define endl "\n"
+#define sd(val) scanf("%d",&val)
+#define ss(val) scanf("%s",&val)
+#define sl(val) scanf("%lld",&val)
+#define debug(val) printf("check%d\n",val)
+#define all(v) v.begin(),v.end()
+#define PB push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define ll long long
+#define MOD 1000000007
+#define clr(val) memset(val,0,sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl; 
+#define OJ \
+	freopen("input.txt", "r", stdin); \
+	freopen("output.txt", "w", stdout);
+#define FIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main(){
+	OJ;
+	FIO;
+	string a,b;
+	cin>>a>>b;
+	if(a==b){// 1. compare if both string are equal or not if equal return -1
+		cout<<"-1\n";
+	}
+	else {//2. now check for longer string and thats our ans
+		cout<<max(a.length(),b.length())<<endl;
+	}
+}
